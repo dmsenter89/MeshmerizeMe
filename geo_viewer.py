519d3bc1a144be613f53aba70ded2d0e96ae257e
Michael Senter <dmsenter89@gmail.com>
2016-10-17 21:43:59 -0400

    First Version GEO_VIEWER
    First version of geo_viewer finished.

--- geo_viewer.py
+++ geo_viewer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
+import argparse
 import matplotlib.pyplot as plt
 #from PyQt5.QtGui import *
 #from PyQt5.QWidget import *
-#from geo_obj import *
+from svg_parser import get_sim_parameters
 
 def read_vertices(fname):
     """
@@ -24,11 +24,39 @@ def read_vertices(fname):
         i += 1
     return vec
 
-def plot_points(vec):
+def plot_points(vec, params):
     """
     Plots vec as a scatter plot.
     """
     x = [elem.real for elem in vec]
     y = [elem.imag for elem in vec]
     plt.scatter(x, y)
+    title_string = params['SimName'] + ' Experiment'
+    plt.title(title_string)
+    plt.xlabel('Width in meters')
+    plt.ylabel('Height in meters')
+    plt.axis([0, params['Lx']*1.1, 0, params['Ly']*1.1])
+    plt.grid(True)
     plt.show()
+
+def main(files):
+    print('Got the files, getting started.')
+    params = {}
+    params = get_sim_parameters(files['parameters'], params)
+    vec = read_vertices(files['vertex'])
+    plot_points(vec, params)
+    print('File Plotted. Thank you.')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Script to visualize a '
+                                    '.vertex file in matplotlib.')
+    parser.add_argument('-f', '--file', required=True,
+                        help='Path to .vertex file')
+    parser.add_argument('-p', '--param', required=True,
+                        help='Path to input2d')
+    args = parser.parse_args()
+    if args.file:
+        files = {'vertex': args.file, 'parameters': args.param}
+        main(files)
+    else:
+        print('GUI version not implemented yet. Use terminal. See -h.')
