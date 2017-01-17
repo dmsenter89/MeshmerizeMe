317a1abbb6399468917decaa3d4dbb732f0ab1c7
Michael Senter <dmsenter89@gmail.com>
2017-01-16 21:19:27 -0500

    Add CLI mode
    Edited MeshmerizeMe to give the user the option of starting the software
    either in GUI or CLI mode. The CLI mode was written to mirror the
    actions taken in the `load_file` function of the GUI class, but printing
    responses to the terminal instead of the GUI. The CLI `main` file
    requires no user intervention.

--- MeshmerizeMe.py
+++ MeshmerizeMe.py
@@ -4,6 +4,7 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+import argparse
 import uidesign as ui
 from svg_parser import *
 from input_parser import fetch_input_params
@@ -25,9 +26,6 @@ class App(QMainWindow, ui.Ui_MainWindow):
         fname_svg = QFileDialog.getOpenFileName(self, "Open SVG File", '',
                                             "Vector Image(*.svg);;All Files (*)")
         fpath, svg_name = os.path.split(fname_svg[0])
-#        fname_param = QFileDialog.getOpenFileName(self,
-#                                            "Open input2d File", '',
-#                                            "All Files (*)")
         self.textEdit.append('Loaded SVG and input2d files.')
         self.labTitle.setText('Thanks. Meshmerizing now.')
         all_paths, params = get_paths(fname_svg[0])
@@ -53,7 +51,7 @@ class App(QMainWindow, ui.Ui_MainWindow):
         self.textEdit.setText("Opened {}.".format(fnamepath[0]))
         finput2d = os.path.join(path,'input2d')
         self.textEdit.append("Begin processing parameters "
-                            "from {}.".format(finput2d))        
+                            "from {}.".format(finput2d))
         params = fetch_input_params(finput2d)
         vec = read_vertices(fnamepath[0])
         plot_points(vec, params)
@@ -71,11 +69,45 @@ class App(QMainWindow, ui.Ui_MainWindow):
     def close_app(self):
         sys.exit()
 
-def main():
+def mainGUI():
     app = QApplication(sys.argv)
     form = App()
     form.show()
     app.exec_()
 
+def main(args):
+    print("\nMeshmerizeMe was started in CLI mode.")
+    fpath, svg_name = os.path.split(args.svgfile)
+    print("You supplied {} as the SVG file to be processed.".format(svg_name))
+    all_paths, params = get_paths(args.svgfile)
+    print("Successfully extracted {} path(s) from the image.".format(
+                                                                len(all_paths)))
+    finput2d = os.path.join(fpath, 'input2d')
+    params = fetch_input_params(finput2d, params)
+    print("Successfully loaded simulation parameters from {}.".format(
+                                finput2d))
+    vertices = make_vertices(all_paths, params)
+    outFile = os.path.join(fpath,params['string_name'])
+    writeFile(outFile, vertices)
+    print("Vertices have been written to {}.vertex.".format(outFile))
+    print("Please verify your files for integrity. Thank you for "
+          "using MeshmerizeMe.")
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description = "Welcome to MeshmerizeMe. "
+                "MeshmerizeMe is a Python script intended to assist with "
+                "creating geometries for fluid simulations using IBAMR and "
+                "IB2d. It uses a user-supplied SVG file and input2d file to "
+                "create .vertex files.",
+                epilog = "Note that the svgfile argument is optional. If no "
+                "svg file is specified on the commandline, the program will "
+                "start in GUI mode. If the user supplies the path to an SVG "
+                "file on the commandline, MeshmerizeMe will directly proceed "
+                "to create the .vertex file.")
+    parser.add_argument('svgfile', nargs='?',
+                help="Path to SVG file for conversion.")
+    args = parser.parse_args()
+    if args.svgfile:
+        main(args)
+    else:
+        mainGUI()
