93f2268a0a4fc058667b4afbbdcae4afa4d2dc0c
Christopher Strickland <mountaindust@gmail.com>
2017-01-23 11:10:55 -0500

    Removes non-Qt wildcard imports (PEP 8)
    "Wildcard imports (from <module> import *) should be avoided, as they make
    it unclear which names are present in the namespace, confusing both
    readers and many automated tools. There is one defensible use case for a
    wildcard import, which is to republish an internal interface as part of a
    public API (for example, overwriting a pure Python implementation of an
    interface with the definitions from an optional accelerator module and
    exactly which definitions will be overwritten isn't known in advance)."
    
    Particularly bad in this case was that outFile from geo_obj was imported
    to MeshmerizeMe via a wildcard import of svg_parser, totally obscuring the
    location of its implementation, and os was imported through a wildcard
    import of geo_viewer. This kind of thing quickly results in spaghetti
    code. Wildcard imports of PyQt5.QtGui and PyQt5.QtWidgets are likely best
    avoided as well, but I will not address them with this commit.

--- MeshmerizeMe.py
+++ MeshmerizeMe.py
@@ -3,12 +3,13 @@
 
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-import sys
+import sys, os
 import argparse
 import uidesign as ui
-from svg_parser import *
+import svg_parser
 from input_parser import fetch_input_params
-from geo_viewer import *
+import geo_viewer
+from geo_obj import writeFile
 
 class App(QMainWindow, ui.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -28,14 +29,14 @@ class App(QMainWindow, ui.Ui_MainWindow):
         fpath, svg_name = os.path.split(fname_svg[0])
         self.textEdit.append('Loaded SVG and input2d files.')
         self.labTitle.setText('Thanks. Meshmerizing now.')
-        all_paths, params = get_paths(fname_svg[0])
+        all_paths, params = svg_parser.get_paths(fname_svg[0])
         self.textEdit.append('Successfully loaded {} path(s) '
                              'from image.'.format(len(all_paths)))
         finput2d = os.path.join(fpath, 'input2d')
         params = fetch_input_params(finput2d, params)
         self.textEdit.append('Loaded simulation parameters '
                             'from {}.'.format(finput2d))
-        vertices = make_vertices(all_paths, params)
+        vertices = svg_parser.make_vertices(all_paths, params)
         self.textEdit.append('Vertices created.')
         outFile = os.path.join(fpath,params['string_name'])
         writeFile(outFile, vertices)
@@ -53,8 +54,8 @@ class App(QMainWindow, ui.Ui_MainWindow):
         self.textEdit.append("Begin processing parameters "
                             "from {}.".format(finput2d))
         params = fetch_input_params(finput2d)
-        vec = read_vertices(fnamepath[0])
-        plot_points(vec, params)
+        vec = geo_viewer.read_vertices(fnamepath[0])
+        geo_viewer.plot_points(vec, params)
         self.textEdit.append("Plotting completed.")
 
     def aboutDiag(self):
@@ -79,14 +80,14 @@ def main(args):
     print("\nMeshmerizeMe was started in CLI mode.")
     fpath, svg_name = os.path.split(args.svgfile)
     print("You supplied {} as the SVG file to be processed.".format(svg_name))
-    all_paths, params = get_paths(args.svgfile)
+    all_paths, params = svg_parser.get_paths(args.svgfile)
     print("Successfully extracted {} path(s) from the image.".format(
                                                                 len(all_paths)))
     finput2d = os.path.join(fpath, 'input2d')
     params = fetch_input_params(finput2d, params)
     print("Successfully loaded simulation parameters from {}.".format(
                                 finput2d))
-    vertices = make_vertices(all_paths, params)
+    vertices = svg_parser.make_vertices(all_paths, params)
     outFile = os.path.join(fpath,params['string_name'])
     writeFile(outFile, vertices)
     print("Vertices have been written to {}.vertex.".format(outFile))
