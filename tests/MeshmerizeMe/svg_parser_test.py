7241483d03e4c28f815d51fd25a60d526a7361ee
Steven Thomas <stevent3115@gmail.com>
2019-06-27 17:10:36 -0400

    Added unimplemented tests.

---
+++ tests/MeshmerizeMe/svg_parser_test.py
@@ -0,0 +1,58 @@
+
+from ...MeshmerizeMe import svg_parser
+
+
+def test_get_paths():
+    assert False, "This test is unimplemented."
+
+def test_get_sim_parameters():
+    assert False, "This test is unimplemented."
+
+def test_coord_transform():
+    assert False, "This test is unimplemented."
+
+def test_points_on_path():
+    assert False, "This test is unimplemented."
+
+def test_make_vertices():
+    assert False, "This test is unimplemented."
+
+def test_chk_vertex_dist():
+    assert False, "This test is unimplemented."
+
+
+
+def test_Space___init__():
+    assert False, "This test is unimplemented."
+
+def test_Space_get_origin():
+    assert False, "This test is unimplemented."
+
+def test_Space_get_max_size():
+    assert False, "This test is unimplemented."
+
+
+
+def test_Svg___init__():
+    assert False, "This test is unimplemented."
+
+def test_Svg_find_space():
+    assert False, "This test is unimplemented."
+
+def test_Svg_find_objects():
+    assert False, "This test is unimplemented."
+
+def test_Svg_get_paths():
+    assert False, "This test is unimplemented."
+
+
+
+def test_SvgObject___init__():
+    assert False, "This test is unimplemented."
+
+def test_SvgObject_get():
+    assert False, "This test is unimplemented."
+
+def test_SvgObject_print_object():
+    assert False, "This test is unimplemented."
+
